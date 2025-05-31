7782d192012b44f8071326b27c4bd0e59f54e274
Youdahe Asfaw <youdaheasfaw@gmail.com>
2025-05-30 20:17:37 -0500

    finshed about page and added js funcitonaly to blog page

--- src/main.js
+++ src/main.js
@@ -7,19 +7,18 @@ import { ContactPage } from './pages/contact';
 let p = 0
 let count = 0
 const AboutShow = document.getElementById('About')
-//Block Content
-// //document.getElementById('pfp').src = user[0].pfp;
-// document.getElementById('name').textContent = user[0].name;
-// document.getElementById('bio').textContent = user[0].bio
-// document.getElementById('title').textContent = posts[p].title
-// document.getElementById('post').textContent = `Post:${posts[p].id}`
-// document.getElementById('content').textContent = posts[p].content
-// document.getElementById('date').textContent = `Date Posted: ${posts[p].date}`
-// let postTag = ''
-// posts[p].tags.forEach(tag => {
-//     postTag += `#${tag}` + " "
-// })
-// document.getElementById("tags").textContent = `Tags: ${postTag}`
+//document.getElementById('pfp').src = user[0].pfp;
+document.getElementById('name').textContent = user[0].name;
+document.getElementById('bio').textContent = user[0].bio
+document.getElementById('title').textContent = posts[p].title
+document.getElementById('post').textContent = `Post:${posts[p].id}`
+document.getElementById('content').textContent = posts[p].content
+document.getElementById('date').textContent = `Date Posted: ${posts[p].date}`
+let postTag = ''
+posts[p].tags.forEach(tag => {
+    postTag += `#${tag}` + " "
+})
+document.getElementById("tags").textContent = `Tags: ${postTag}`
 
 function Menu(){
     const arrow = document.querySelector('#arrow img');
